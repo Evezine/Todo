f51f8fee83021bc9aabbeec146fe54bce8c81fae
viswa <viswa.r2002@gmail.com>
2024-09-11 10:36:43 +0530

    todo app final

--- todos/views.py
+++ todos/views.py
@@ -12,3 +12,27 @@ def mark_as_done(request,pk):
     task.is_completed =True
     task.save()
     return redirect('home')
+
+def mark_as_undone(request,pk):
+    task =get_object_or_404(Task, pk=pk)
+    task.is_completed =False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task =get_object_or_404(Task, pk=pk) # get request to load the page or get the page ,POST is used to update or add the data
+    if request.method =='POST':
+        new_task= request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context ={
+             'get_task':get_task,       
+        }
+        return render(request, 'edit_task.html',context)
+    
+def delete_task(requet,pk):
+    task =get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
